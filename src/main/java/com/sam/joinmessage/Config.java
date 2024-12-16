7ad72d99b0a7e396fd1f7010bc235fdb3dc27b5d
Samotage820 <samjirik@gmail.com>
2024-12-16 17:40:27 -0600

    first working version

---
+++ src/main/java/com/sam/joinmessage/Config.java
@@ -0,0 +1,52 @@
+package com.sam.joinmessage;
+
+import com.google.gson.Gson;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+public class Config {
+    public static File configFile;
+
+    public static class Messages {
+        public String welcome;
+        public List<String> random;
+    }
+
+    public static void createConfig() {
+        configFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), JoinMessage.MOD_ID + ".json");
+        try {
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream("default_config.json");
+            InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+
+            Gson gson = new Gson();
+            Messages defaultMessages = gson.fromJson(streamReader, Messages.class);
+            streamReader.close();
+            Writer writer = new FileWriter(configFile);
+            gson.toJson(defaultMessages, writer);
+            writer.close();
+        } catch (IOException e) {
+            JoinMessage.LOGGER.error("Couldn't save Join Message config.");
+        }
+    }
+
+    public static Messages loadConfig() {
+        try {
+            if (configFile == null) {
+                createConfig();
+            }
+
+            Reader reader = new FileReader(configFile);
+            Gson gson = new Gson();
+            Messages messages = gson.fromJson(reader, Messages.class);
+            reader.close();
+            return messages;
+        } catch (IOException e) {
+            JoinMessage.LOGGER.error("Couldn't load Join Message config.");
+        }
+        return new Messages();
+    }
+}
---
+++ src/main/java/com/sam/joinmessage/JoinMessage.java
@@ -0,0 +1,49 @@
+package com.sam.joinmessage;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
+public class JoinMessage implements ModInitializer {
+	public static final String MOD_ID = "join-message";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		Config.Messages messages = Config.loadConfig();
+		if (messages == null) {
+			LOGGER.error("Join Message failed to initialize.");
+			return;
+		}
+
+		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
+			ServerPlayerEntity player = handler.getPlayer();
+			if (messages.welcome != null) {
+				player.sendMessage(Text.literal(messages.welcome).formatted(Formatting.GREEN));
+			}
+			if (!messages.random.isEmpty()) {
+				Random random = new Random();
+				int randInt = random.nextInt(messages.random.size());
+				player.sendMessage(Text.literal(messages.random.get(randInt)).formatted(Formatting.GREEN, Formatting.ITALIC));
+			}
+			LOGGER.info("Sent join message to {}", handler.getPlayer().getName());
+		});
+
+		LOGGER.info("Join Message initialized.");
+	}
+}
